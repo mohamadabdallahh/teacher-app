d1c023b049ee520f3ea6404425e384912a1c17cb
mohamadabdallahh <anghamiformohamad2@gmail.com>
2026-05-09 14:20:21 +0300

    Update: Sort students alphabetically and fix API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API = process.env.REACT_APP_API_URL ;
 console.log("API set to:", API);
 
 export default function App() {
